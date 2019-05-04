43bd3756d3ec1b05027ed342b684bf37e598dfaf
suhanyujie <suhanyujie@qq.com>
2019-05-04 19:03:46 +0800

    add swoole clalass

---
+++ app/Services/Swoole/Websocket.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samuel
+ * Date: 2019/5/4
+ * Time: 19:00
+ */
+
+namespace App\Services\Swoole;
+
+
+class Websocket
+{
+    protected $sv;
+
+    public function __construct()
+    {
+        
+    }
+}

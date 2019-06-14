df93fee374b7be0999e1bbdfee8302cb0b816fe0
suhanyujie <suhanyujie@qq.com>
2019-06-14 18:43:16 +0800

    调整初始迁移表的一些结构 调整 composer install 后的一些初始化命令

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ss-message
 * 预计是一个沟通工具
 
+## install
+
+
 ## 依赖
 * [laravel-admin](https://github.com/z-song/laravel-admin)
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 20);
+            $table->string('real_name', 50);
+            $table->tinyInteger('gender', false, true);
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60);
+            $table->string('pic', 150);
             $table->rememberToken();
             $table->timestamps();
         });
